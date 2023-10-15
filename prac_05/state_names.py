2956c3565e506c9f3e047b28272ccf3ea4de1f64
Rusty <rustyjohn.balatan@my.jcu.edu.au>
2023-10-15 16:22:24 +1000

    prac 5, complete task 1 with the reformatted code

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -6,6 +6,7 @@ File needs reformatting
 
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+
 def main():
     print(CODE_TO_NAME)
     while True:
