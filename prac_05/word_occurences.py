67f34f402f73fe4f15ec5e7cb29eb38d9616ccc6
Rusty <rustyjohn.balatan@my.jcu.edu.au>
2023-10-15 19:19:39 +1000

    prac 5, complete task 3, with word occurence task implemented and the sorting and spacing requirement.

---
+++ prac_05/word_occurences.py
@@ -0,0 +1,45 @@
+"""
+- Program to count the occurrences of words in a string.
+- Asks for a string, prints the counts of how many each
+word in the string.
+- this program is case-insensitive
+
+estimate: 1 hr
+actual:
+
+"""
+
+
+
+def main():
+    # get input string, convert to list
+    string = input("Enter Text\n>>>")
+    words = string.split()
+    # get longest word length
+    longest_length = find_long_word(words)
+    words_to_count = {}  # pre-allocate
+    # from the slides
+    for word in words:
+        word = word.lower()
+        if word in words_to_count:
+            words_to_count[word] += 1
+        else:
+            words_to_count[word] = 1
+
+    # print results
+    words_to_count = sorted(words_to_count.items())
+    for word, count in words_to_count:
+        print(f"{word:{longest_length}} = {count}")
+
+#function to find the longest word length with in the string
+def find_long_word(words):
+    word_length = []
+    for word in words:
+        length = len(word)
+        word_length.append(length)
+
+    longest_length = max(word_length)
+
+    return longest_length
+
+main()
