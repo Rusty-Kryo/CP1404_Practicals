7963addcd848530dacad2ba6acf3c1236f1614ba
Rusty <rustyjohn.balatan@my.jcu.edu.au>
2023-10-15 16:06:45 +1000

    prac 5, task 1 initial commit

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -7,13 +7,13 @@ File needs reformatting
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD":"Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
             "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-def main():
-    print(CODE_TO_NAME)
 
-    state_code = input("Enter short state: ")
-    while state_code != "":
-        if state_code in CODE_TO_NAME:
-            print(state_code, "is", CODE_TO_NAME[state_code])
-        else:
-            print("Invalid short state")
-            state_code = input("Enter short state: ")
+print(CODE_TO_NAME)
+
+state_code = input("Enter short state: ")
+while state_code != "":
+    if state_code in CODE_TO_NAME:
+        print(state_code, "is", CODE_TO_NAME[state_code])
+    else:
+        print("Invalid short state")
+        state_code = input("Enter short state: ")
