39f400a7de4945c9fba1b7c4abe7792a0572e88f
Rusty <rustyjohn.balatan@my.jcu.edu.au>
2023-11-19 17:45:27 +1000

    Week 9 practical, completed unreliable car task. Including the implementation of unreliable car class program and unreliable car test program.

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -8,7 +8,7 @@ from prac_09.unreliable_car import UnreliableCar
 
 
 def main():
-    """Test the unreliable car class by running the following code"""
+    """Test the unreliable car class by running the following code."""
     for i in range(1, 100):
         my_car = UnreliableCar("Prius", 100, 50)
         my_car.drive(40)
