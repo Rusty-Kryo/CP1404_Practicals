bbdddbb806dccbd2ff9a5aa7f142ffcdbced407b
Rusty <rustyjohn.balatan@my.jcu.edu.au>
2023-11-19 17:45:08 +1000

    Week 9 practical, completed unreliable car task. Including the implementation of unreliable car class program and unreliable car test program.

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -9,25 +9,26 @@ week 9 coding practical.
 import random
 from prac_09.car import Car
 
+
 class UnreliableCar(Car):
     def __init__(self, name, fuel, reliability):
         """Initialise an unreliable car instance.
-            reliability is a float, the chance it will drive
+            reliability is a float, the chance it will drive.
         """
         super().__init__(fuel, name)
         self.reliability = reliability
 
     def __str__(self):
-        """Returns a string like car but with reliability"""
+        """Returns a string like car but with reliability."""
         return f"{super().__str__()}, Reliability:{self.reliability}"
 
     def drive(self, distance):
         """Drive the car like the car class, with the addition of checking
-        if it will drive or not"""
+        if it will drive or not."""
         will_it_drive = random.randint(0, 101)
         if will_it_drive < self.reliability:
             distance_driven = super().drive(distance)
             return distance_driven
+
         else:
             print("Car Did Not Drive!!")
-
